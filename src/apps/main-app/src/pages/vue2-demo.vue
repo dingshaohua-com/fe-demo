bca674bbe742bc1b6888e72024210979ed978577
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-05 18:13:54 +0800

    fix: preview

--- src/apps/main-app/src/pages/vue2-demo.vue
+++ src/apps/main-app/src/pages/vue2-demo.vue
@@ -1,3 +1,9 @@
 <template>
-    <micro-app name="vue2-demo" url="http://localhost:8080/"/>
+    <!-- <micro-app name="vue2-demo" url="http://localhost:8080/"/> -->
+    <micro-app name="vue2-demo" :url="`${origin}/vue2-demo`"/>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const origin = ref(window.location.origin);
+</script>
--- src/apps/main-app/src/pages/vue3-demo.vue
+++ src/apps/main-app/src/pages/vue3-demo.vue
@@ -1,3 +1,9 @@
 <template>
-    <micro-app name="vue3-demo" url="http://localhost:8081/" iframe/>
+    <!-- <micro-app name="vue3-demo" url="http://localhost:8081/" iframe/> -->
+    <micro-app name="vue3-demo" :url="`${origin}/vue3-demo`" iframe/>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const origin = ref(window.location.origin);
+</script>
