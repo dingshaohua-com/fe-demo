3af6c53a9e4e3d446e79c374f4dce9e9b3b18d48
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-05 18:55:50 +0800

    fix

--- src/apps/main-app/src/pages/vue3-demo.vue
+++ src/apps/main-app/src/pages/vue3-demo.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <micro-app name="vue3-demo" url="http://localhost:8081/" iframe/> -->
-    <micro-app name="vue3-demo" :url="`${origin}/vue3-demo`" iframe/>
+    <micro-app name="vue3-demo" :url="`${origin}/vue3-demo/index.html`" iframe/>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
